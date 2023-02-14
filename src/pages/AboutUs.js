a1ea511f0b182a485cdaa4481e06d4c209df7d0e
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-02-14 14:44:57 +0530

    about page fixed

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,32 +1,5 @@
-import { Button } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React from "react";
 
 export default function AboutUs() {
-  const [searchText, setSearchText] = useState("");
-
-  const handleSearch = () => {
-    const elements = document.querySelectorAll("*");
-    // console.log(elements);
-    elements.forEach((element) => {
-      if (element.innerHTML.includes(searchText)) {
-        // element.style.backgroundColor = "yellow";
-        console.log(element);
-      }
-    });
-  };
-
-  return (
-    <div className="w-full h-full flex flex-col justify-center items-center">
-      <input
-        type="text"
-        value={searchText}
-        onChange={(e) => setSearchText(e.target.value)}
-        className="border border-gray-200 rounded"
-      />
-      <Button colorScheme="green" onClick={handleSearch}>
-        Search
-      </Button>
-      <p>Hi this is Yuvraj singh</p>
-    </div>
-  );
+  return <div className="">About</div>;
 }
